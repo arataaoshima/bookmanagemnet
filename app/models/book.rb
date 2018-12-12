43a9365ebac7c7b5f21852d738e1405ef8bb223c
ARATA <newbluelight1031@gmail.com>
2018-12-12 00:12:35 +0000

    update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ActiveRecord::Base
-    has_many :reservations
-    has_many :users, through: :reservations
+     has_many :reservations
+     has_many :users, through: :reservations
+     
+    def self.search(search) #self.でクラスメソッドとしている
+     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      Book.where(['title LIKE ?', "%#{search}%"])
+     else
+      Book.all #全て表示。
+     end
+    end
 end
